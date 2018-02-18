bbfaaf8ea150c73fed5a91ef0e3911075bc98314
Jegan Matthews <matthewsjegan@gmail.com>
2018-02-17 16:03:05 -0800

    beta test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Jegan\'s Docker Test' });
+  res.render('index', { title: 'Jegan\'s Docker Test (beta)' });
 });
 
 module.exports = router;
